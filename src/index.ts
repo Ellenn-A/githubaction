8412c5b1504f1102eb4b5ca1c909ce78799a96fb
Helena Adamkova <adamkova.helena.ghb@gmail.com>
2023-08-13 15:38:35 +0100

    greeting Helenki

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello world!')
+import { getInput } from "@actions/core"
+
+const inputName = getInput('name')
+
+greet(inputName)
+
+function greet(name:string){
+    console.log('Hello ${name}!')
+}
+
