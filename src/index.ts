e7c2a4f8bfd7fae50da5142c3481dc5d0ab57c89
Helena Adamkova <adamkova.helena.ghb@gmail.com>
2023-08-13 15:47:21 +0100

    retrieve repo metadata

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import { getInput } from "@actions/core"
+import {context} from "@actions/github"
+
+type GithubContext = typeof context
 
 const inputName = getInput('name')
 
-greet(inputName)
+greet(inputName, getRepoURL(context))
 
-function greet(name:string){
-    console.log(`Hello ${name}!`)
+function greet(name:string, repoUrl: string){
+    console.log(`Hello ${name}! You are running Github Action in ${repoUrl}`)
 }
 
+function getRepoURL({repo, serverUrl}:GithubContext):string{
+    return `${serverUrl}/${repo.owner}/${repo.repo}`
+}
