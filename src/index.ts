bde26c4864773d881e6358ab7ed950a1394da8b5
Helena Adamkova <adamkova.helena.ghb@gmail.com>
2023-08-13 15:53:23 +0100

    print whole payload

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,15 @@ const inputName = getInput('name')
 
 greet(inputName, getRepoURL(context))
 
+console.log('Action info -----------')
+console.log('eventname: ', context.eventName)
+console.log('sha: ', context.sha)
+console.log('ref: ', context.ref)
+console.log('workflow: ', context.workflow)
+console.log('job: ', context.job)
+
+console.log(JSON.stringify(context.payload, undefined,2))
+
 function greet(name:string, repoUrl: string){
     console.log(`Hello ${name}! You are running Github Action in ${repoUrl}`)
 }
